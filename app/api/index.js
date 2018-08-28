1b84bf9e5ab65b0efdc0a351e25069f322210fcd
Hector Aristy <rafelito266@gmail.com>
2018-08-27 21:59:36 -0400

    remove riot games api and use ddragon static data to bring all information in this way we dont need an api key

--- app/api/index.js
+++ app/api/index.js
@@ -3,13 +3,11 @@
  *
  * @author Kelvin De Moya <http://github.com/kdemoya>.
  */
-'use strict';
 
 import * as actions from '../actions/champions';
-import { riotApi } from '../constants/configs';
 
 const getVersion = (store) => {
-  fetch(`https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key=${riotApi.development_key}`)
+  fetch(`https://ddragon.leagueoflegends.com/api/versions.json`)
     .then((result) => {
       result.json().then((versions) => {
         const currentVersion = versions[0];
@@ -35,7 +33,7 @@ export const RiotAPI = {
   getAllChamps: (store) => {
     getVersion(store);
 
-    fetch(`https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=${riotApi.development_key}`)
+    fetch(`https://ddragon.leagueoflegends.com/cdn/8.17.1/data/en_US/champion.json`)
       .then((result) => {
         result.json().then((champions) => {
           store.dispatch(actions.fetchSuccess(champions.data));
@@ -58,10 +56,10 @@ export const RiotAPI = {
    * @param {Object} Actions    - Actions object.
    */
   fetchSingle: (champId, dispatch, Actions) => {
-    fetch('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + champId + '?champData=info,spells,lore,skins,stats&api_key=' + riotApi.development_key)
+    fetch(`https://ddragon.leagueoflegends.com/cdn/8.17.1/data/en_US/champion/${champId}.json`)
       .then((result) => {
         result.json().then((champion) => {
-          dispatch(actions.fetchSingleSuccess(champion));
+          dispatch(actions.fetchSingleSuccess(champion.data[champId]));
         }).catch((error) => {
           console.log(error);
         });
